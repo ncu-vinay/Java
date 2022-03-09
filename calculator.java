05b8b49476323aac0e4afc85cd7d3439892de408
Vinay Kumar <101244361+ncu-vinay@users.noreply.github.com>
2022-03-09 13:50:50 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,17 @@
+class calculation
+{
+  public static void main(String args[]) 
+  {
+    int sum, sub, div, mul, r, a;
+    r = 100;
+    a = 10;
+    sum = a+r;
+    sub = r-a;
+    div = r/a;
+    mul = r*a;
+    System.out.println("Addition - " + sum);
+    System.out.println("Subtraction - " + sub);
+    System.out.println("Multiplication - " + div);
+    System.out.println("Division - " + div);
+  }
+}
