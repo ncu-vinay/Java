674c3caf5fce9d206bda84bae5111a62d7fb5c5d
Vinay Kumar <101244361+ncu-vinay@users.noreply.github.com>
2022-03-22 09:56:17 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,17 @@
+class Pattern
+{
+  public static void main(String args[]) 
+  {
+      int n = 6;
+      for(int i=0;i<n;i++)
+      {
+          // printing '*' in each column.
+          for(int j=0;j<=i;j++)
+          {
+            System.out.print("* ");
+          }
+          System.out.println();
+      }
+  }
+}  
+ 
