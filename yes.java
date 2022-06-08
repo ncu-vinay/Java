75e713c7ee5d5ee3250a9ba05cf0e6629b5b9997
Vinay Kumar <101244361+ncu-vinay@users.noreply.github.com>
2022-06-08 11:22:52 +0530

    Interfaces  area perimeter
     Define the interface / class hierarchy as detailed in the following class diagram 
         Definition of Done:
         DOD 1:     The class definitions are defined as per the class diagram.
         DOD 2:  Each class definition is stored in its own .java file.
         DOD 3:   Base class constructors are invoked using super keyword   
         DOD 4:   Function overriding is applied wherever applicable.

--- yes.java
+++ yes.java
@@ -1,19 +1,48 @@
-class Pattern
+class Resizeablecircle  extends circle implements Resizeable 
 {
-  public static void main(String []arr) 
-  {
-   int Lspace;
-   for(int i=1;i<=4;i++)
-   {
-    for(Lspace=1;Lspace<=4-i;Lspace++)
-    {
-     System.out.print(" ");
-    }
-    for(int col=1;col<=7-2*(Lspace-1);col++)
-    {
-     System.out.print("*");
-    }
-    System.out.println( );
-   }
+ Resizeablecircle(double r)
+ {
+  super(r);
+ }
+ //@override
+ public void resize(int percent)
+ {
+ }
+}
+interface Resizeable
+{
+ public void resize(int percent);
+}
+class circle implements geometricobject
+{
+ double radius;
+ circle(double radius)
+ {
+  this.radius = radius;
+ }
+ public double getperimeter()
+ {
+  double peri = 2*3.14*radius;
+  return peri;
+ }
+ public double getarea()
+ {
+  double area = 3.14*radius*radius;
+  return area;
+ }
+}
+interface geometricobject
+{
+ public double getperimeter();
+ public double getarea();
+}
+class main
+{
+ public static void main(String[] args) 
+ {
+  Resizeablecircle obj = new Resizeablecircle(1.0);  
+    System.out.println(obj.getperimeter());
+    System.out.println(obj.getarea());
+    
   }
 }
